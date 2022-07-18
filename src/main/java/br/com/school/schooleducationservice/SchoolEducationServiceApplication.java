a9ba2173876ddcbe41a7fb5fc2dda3ad939b02f9
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-07-18 20:07:57 -0300

    Commit base do projeto.

---
+++ src/main/java/br/com/school/schooleducationservice/SchoolEducationServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.school.schooleducationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolEducationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolEducationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/school/schooleducationservice/SchoolEducationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.school.schooleducationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolEducationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
